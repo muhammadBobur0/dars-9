348e0d2f17ca898d92b511c952e1df0f6a47e0d5
muhammadBobur0 <muhammadboburbek@gmail.com>
2022-11-21 19:08:32 +0500

    news

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 function httpServer (req, res) {
   const app = new Express(req, res)
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET','POST', 'DELETE', 'PUT')
+  res.setHeader('Access-Control-Allow-Methods', '*')
 
   app.get('/todos', (req, res) => {
     let { completed } = req.query
